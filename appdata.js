89e695ad5df4fe3974d12e20a8ffdb6d3b6fe48d
Antony Tomy <47214790+qnxdev@users.noreply.github.com>
2020-12-01 09:55:07 +0530

    Update appdata.js

--- appdata.js
+++ appdata.js
@@ -236,7 +236,7 @@ export const AppData =
         "icon": "Bird10.png",
         "vendor": "Simone Dassi",
         "rating": 5,
-        "version": "2.6.0.0",
+        "version": "2.7.0.0",
         "description": "Bird10 Native Twitter client for BlackBerry 10.",
         "reviews": [
             {
