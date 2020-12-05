5b9f8b601b6f0e096b26dca17200d5f5d570edcd
Antony Tomy <47214790+qnxdev@users.noreply.github.com>
2020-12-05 09:52:59 +0530

    Update listing.js

--- pages/api/listing.js
+++ pages/api/listing.js
@@ -1,6 +1,44 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { firebaseInit } from "../../lib/firebase";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'App Name' })
+export default async (req, res) => {
+  const db = await firebaseInit();
+  if (req.query.v == "apps") {
+    let apps = [];
+      const querySnapshot = await db
+        .firestore()
+        .collection("apps")
+        .get();
+      querySnapshot.forEach(doc => {
+        apps.push(doc.data());
+      });
+      res.json({ apps });
+  }
+
+  else if (req.query.v == "tools") {
+    let tools = [];
+    const querySnapshot = await db
+      .firestore()
+      .collection("tools")
+      .get();
+    querySnapshot.forEach(doc => {
+      tools.push(doc.data());
+    });
+    res.send({ tools });
+  }
+
+
+  else if (req.query.v == "games") {
+    let games = [];
+    const querySnapshot = await db
+      .firestore()
+      .collection("games")
+      .get();
+    querySnapshot.forEach(doc => {
+      contracts.push(doc.data());
+    });
+    res.send({ games });
+  }
+  else {
+    res.status(403).end("403 | Forbidden");
+  }
 }
